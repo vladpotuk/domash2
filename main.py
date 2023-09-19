603fac96463130b8240c73834e05368520ca7681
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2023-09-19 20:38:49 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+a=int(input())
+if a>0:
+    print("Number is positive")
+elif a<0:
+    print("Number is negative")
+elif a==0:
+    print("Number is equal to zero")
+else:
+    print("Error")
+
+
+
